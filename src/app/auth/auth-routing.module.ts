234836da4f3c1c97c598701e5295a51d17d36077
Murayzes <murayzes@gmail.com>
2019-02-18 13:27:42 +0200

    i_create_autorization

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,15 @@
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
