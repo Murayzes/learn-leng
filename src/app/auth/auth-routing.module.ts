c14c0ada4366b3297c2253a08d8e275aa77895e9
Murayzes <murayzes@gmail.com>
2019-02-20 16:23:16 +0200

    i_started_to_make_up_login_page

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,9 +1,9 @@
-import { AuthComponent } from './auth.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { AuthComponent } from './auth.component';
 
 const routes: Routes = [
   {path: '', component: AuthComponent, children: [
