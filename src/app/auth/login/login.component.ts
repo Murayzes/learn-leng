9faa41f4db1cbf3e90b38b5eff73500fe5bc1c45
Murayzes <murayzes@gmail.com>
2019-03-06 15:09:21 +0200

    i_created_methods_getUserByEmail_and_showMessage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { User } from './../../shared/models/user.model';
+import { UsersService } from './../../shared/services/users.service';
+import { Message } from '../../shared/models/message.model';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,18 +14,42 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
+  message: Message;
 
-  constructor() {}
+  constructor(
+    private usersService: UsersService
+  ) {}
 
   ngOnInit() {
+    this.message = new Message('dander', '');
     this.form = new FormGroup ({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
     });
   }
 
+  private showMessage(text: string, type: string = 'danger') { // login error message method
+    this.message = new Message(type, text);
+    window.setTimeout(() => {
+      this.message.text = '';
+    }, 5000);
+  }
+
   onSubmit () {
-    console.log(this.form);
+    const formData = this.form.value;
+
+    this.usersService.getUserByEmail(formData.email)
+      .subscribe((user: User) => {
+        if (user) {
+          if (user.password === formData.password) {
+            // logic
+          } else {
+            this.showMessage('Wrong password!');
+          }
+        } else {
+          this.showMessage('This user does not exist!');
+        }
+      });
   }
 
 }
