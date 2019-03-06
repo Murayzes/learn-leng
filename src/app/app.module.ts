ce6f193eb6670ef57807b8efa659d3ac0a994443
Murayzes <murayzes@gmail.com>
2019-03-06 15:02:22 +0200

    i_registered_UserService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersService } from './shared/services/users.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AuthModule,
     AppRoutingModule,
     MatMenuModule
   ],
+  providers: [UsersService],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
