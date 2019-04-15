792ecc35046496a08569a6296bf60a8be1fefa35
Murayzes <murayzes@gmail.com>
2019-04-15 19:25:48 +0300

    i_subscribed_to_the_event_and_create_method_logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './shared/services/auth.service';
+import { User } from './shared/models/user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {  }
+export class AppComponent {
+  currUser: User;
+
+  constructor(
+      private router: Router,
+      private authService: AuthService
+  ) {
+      this.authService.currUser.subscribe(x => this.currUser = x);
+  }
+
+  logout() {
+      this.authService.logout();
+      this.router.navigate(['/login']);
+  }
+}
+
