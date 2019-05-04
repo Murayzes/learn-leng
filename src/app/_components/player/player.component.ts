ab3523d2ba3e5a064e0fd9bb30858389462207a8
Murayzes <murayzes@gmail.com>
2019-05-04 13:48:41 +0300

    created_player_component

---
+++ src/app/_components/player/player.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrls: ['./player.component.css']
+})
+export class PlayerComponent {
+
+  player: YT.Player;
+
+  private id = 'C4m6-UlX7Kg';
+
+  savePlayer(player) {
+    this.player = player;
+    console.log('player instance', player);
+  }
+  onStateChange(event) {
+    console.log('player state', event.data);
+  }
+
+}
